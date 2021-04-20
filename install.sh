56aafdd0c8637e0d3dc8430a347a822d63436310
Aaron Flaszynski <aaron@vistab.co.nz>
2021-04-21 10:57:10 +1200

    Remove git submodules

--- install.sh
+++ install.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-cd ~/dotfiles
-git submodule init
-git submodule update
+SYMLINK_LIST="bash_profile
+colordiffrc
+gitconfig
+gitignore_global
+gvimrc
+inputrc
+npmrc
+pip
+tmux.conf
+vim
+vimrc
+wgetrc
+zshrc"
 
-cd
-ln -nfs ~/dotfiles/vimrc ~/.vimrc
-WORKING_DIR=~/.vim
-if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
-ln -nfs ~/dotfiles/vim ~/.vim
-WORKING_DIR=~/.gitconfig
-if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
-ln -nfs ~/dotfiles/gitconfig ~/.gitconfig
-ln -nfs ~/dotfiles/tmux.conf ~/.tmux.conf
+BASE_DIR=$(cd $(dirname $0); pwd)
+cd $BASE_DIR
+
+for FILE in $SYMLINK_LIST
+do
+  rm -rf ~/.$FILE
+  ln -s $BASE_DIR/$FILE ~/.$FILE
+done
+
+# Nvim config
+ln -nfs $BASE_DIR/config ~/.config
+
+# Install the Vim-Plug Plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install NeoVim Plugins
+mvim +PlugInstall +qall
+
+# MacDown command line
+mkdir ~/bin
+ln -s /Applications/MacDown.app/Contents/SharedSupport/bin/macdown ~/bin
 
 exit 0
